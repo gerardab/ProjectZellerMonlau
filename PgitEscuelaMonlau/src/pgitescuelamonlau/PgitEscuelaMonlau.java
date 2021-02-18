6e0cda7cc39a5da7b7e02aae37c9b4ef273ac0ed
gerardab <gerard_ab@hotmail.com>
2021-02-18 16:30:58 +0100

    first commit

---
+++ PgitEscuelaMonlau/src/pgitescuelamonlau/PgitEscuelaMonlau.java
@@ -0,0 +1,25 @@
+/*
+ 18/02/2021
+ Autor: Monlau 
+ proyecto de Dia internacional de Programador 
+Modificacion: Marc
+ */
+package pgitescuelamonlau;
+
+public class PgitEscuelaMonlau {
+
+    public static void main(String[] args) {
+      int day=13, month=9, year=2021; 
+      String stDat=birthDay_ZellerMonlau(day, month,year);
+      System.out.println("International programmer's day:  "+stDat + " sep2021");
+    }
+    private static String birthDay_ZellerMonlau(int day, int month, int year) {
+        String[] ArrayWeek = {"Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday"};
+        int a, y, m, d;
+        a = (14 - month) / 12;
+        y = year - a;
+        m = month + 12 * a - 2;
+        d = (day + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+        return ArrayWeek[d];
+    } 
+}
